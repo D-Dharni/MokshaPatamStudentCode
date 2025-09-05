a4e6a81b77140a5457e6a53e4878754abe955089
devendharni <deven.dharni@gmail.com>
2025-09-04 18:43:25 -0700

    - Made a series of functions to determine whether the board was winnable

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date        |   Time    |                                                                                                                                                                                                                                                                                                       Update |
+|:------------|:---------:|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| April 5     |  9-10pm   |                                                                                                                                                                                                                                                                   I tried to write abstract, efficient code. |
+| September 4 | 5:15-6:45 | I worked with niam on this project. I created a series of functions that determined whether or not the board was winnable. First I checked if there was a series of 6 snakes in a row and then if there was, if there was a ladder to bypass it. By using those functions we checked if the board was valid. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -1,3 +1,5 @@
+import java.util.Collections;
+
 /**
  * Moksha Patam
  * A puzzle created by Zach Blick
@@ -15,7 +17,84 @@ public class MokshaPatam {
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
+        if (checkIsWinnable(ladders, snakes) == false) {
+            return -1;
+        }
+
+        // make a new array list of ladder values that go up more than siz
+
+        // make a array of snake starting points to avoid them
+
+        // use each ladder and increment by sufficient amount
 
         return 0;
+    }
+
+    public static boolean checkIsWinnable(int[][] ladders, int [][] snakes) {
+        int num = checkSixSnakes(snakes);
+        if (num == -1) {
+            return true;
+        }
+        else {
+            int start = num;
+            int end = num + 5;
+
+            for (int[] ladder: ladders) {
+                int ladderStart = ladder[0];
+                int ladderEnd = ladder[1];
+
+                if (ladderStart < start && ladderEnd > end) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static int checkSixSnakes(int [][] snakes) {
+        if (snakes.length < 6) {
+            return -1;
+        }
+        int[] snakeStartingPoints = new int[snakes.length];
+        for (int i = 0; i < snakes.length; i++) {
+            snakeStartingPoints[i] = snakes[i][0];
+        }
+        bubbleSort(snakeStartingPoints);
+
+        int consecutive = 1;
+
+        for (int i = 1; i < snakeStartingPoints.length; i++) {
+            if (snakeStartingPoints[i] == snakeStartingPoints[i - 1] + 1) {
+                consecutive++;
+                if (consecutive == 6) {
+                    return snakeStartingPoints[i] - 5;
+                }
+            }
+            else {
+                consecutive = 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int length = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < length - 1 - i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
     }
 }
